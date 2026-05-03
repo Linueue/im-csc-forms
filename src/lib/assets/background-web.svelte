a6a1d2ff8bdb0829782174b24d5187a3bdb21f73
Linueue <rikvcelestial@gmail.com>
2026-05-03 15:00:22 +0800

    feat(landing): Added landing page.

---
+++ src/lib/assets/background-web.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { width = "1440", height = "385", fill = "#EC6B6B", ...restProps } = $props();
+</script>
+
+<svg width={width} height={height} {...restProps} viewBox="0 0 1440 385" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M-108 148.751C-75.4607 143.254 915.634 -43.1774 1353.56 9.30314C1703.9 51.2876 1499.53 572.595 1353.56 828L24.869 821.003L-108 148.751Z" fill={fill}/>
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,85 @@
 <script lang="ts">
-    import ApplicationForm from "./application-form/application-form.svelte"
+    import Column from "$lib/components/column.svelte"
+    import * as Alert from "$lib/components/ui/alert/index.js"
+    import { Button } from "$lib/components/ui/button/index.js"
+    import MessageSquareWarningIcon from "@lucide/svelte/icons/message-square-warning"
+    import {goto} from "$app/navigation"
+    import Logo from "$lib/assets/logo.svelte"
+
+    function gotoApply()
+    {
+        goto("/application-form");
+    }
 </script>
 
 <div class="flex flex-row content-container">
+    <Column>
+        <!-- p = padding -->
+        <div class="p-[1em] flex justify-center">
+            <Logo width={"255px"} height={"100px"} />
+        </div>
+        <div class="title-container">
+            <Column>
+                <h1>CSC Professional</h1>
+                <h1>Examination 2026</h1>
+                <h1 class="subtitle">Applications are now open!</h1>
+
+                <!-- w = width, rounded = border-radius, self-center = justify-self: center, m = margin -->
+                <Button class="w-[8em] rounded-2xl self-center m-[1em]" onclick={gotoApply}>Apply</Button>
+                
+                <!-- pt = padding top -->
+                <div class="pt-[5em] self-center w-[80ch]">
+                    <Alert.Root class="shadow-md">
+                        <MessageSquareWarningIcon color="var(--destructive)" />
+                        <Alert.Title>Disclaimer</Alert.Title>
+                        <Alert.Description>
+                            This website is not an official government website, and is only for University purposes. Any data that is collected shall only be for dummy data. 
+                        </Alert.Description>
+                    </Alert.Root>
+                </div>
+            </Column>
+        </div>
+    </Column>
 </div>
 
 <style>
+    .subtitle
+    {
+        padding-top: 3rem;
+        font-size: 1.5rem;
+        width: 100%;
+        align-content: center;
+        letter-spacing: 0px;
+    }
+
+    .title-container
+    {
+        padding: 1.5em 3em;
+    }
+
+    h1
+    {
+        padding: 0em;
+        margin: 0em;
+        line-height: 1.1em;
+        font-size: 4.2em;
+        font-family: "Merriweather Variable", serif;
+        letter-spacing: 2px;
+        text-align: center;
+    }
+
+    .background-static
+    {
+        position: fixed;
+        left: 0;
+        bottom: -50px;
+        overflow: hidden;
+        z-index: 0;
+    }
+
     .content-container
     {
         padding: 1em;
+        background-color: var(--background);
     }
 </style>
